203a96619e2d4f0e447c3d976c70b9396cfd3cae
Thiago Oliveira <oliveira@Oliveira.Oliveira>
2017-05-30 20:35:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/oliveira/salvaaluno/BancoHelper.java
@@ -0,0 +1,34 @@
+package com.example.oliveira.salvaaluno;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by oliveira on 09/05/17.
+ */
+
+public class BancoHelper extends SQLiteOpenHelper {
+    public static String BANCO_NOME = "escola.db";
+    public static int BANCO_VERSAO = 1;
+
+    public BancoHelper(Context context) {
+        super(context, BANCO_NOME,null,BANCO_VERSAO);
+    }
+
+    public BancoHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table atividades(id integer primary key autoincrement, tipo text, data_hora DATETIME, status text);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
