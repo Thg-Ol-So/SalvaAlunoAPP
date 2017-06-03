be97646045bca70e3b7aae643d31984e9412e304
oliveira <thiago-coxim@hotmail.com>
2017-06-03 11:52:59 -0400

    'Calendario '

--- app/src/main/java/com/example/oliveira/salvaaluno/CadastroAtividades.java
+++ app/src/main/java/com/example/oliveira/salvaaluno/CadastroAtividades.java
@@ -2,14 +2,13 @@
 package com.example.oliveira.salvaaluno;
 
 
-import android.app.SearchManager;
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.DialogInterface;
+import android.os.Bundle;
 
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
@@ -39,37 +38,14 @@ public class CadastroAtividades extends AppCompatActivity implements DatePickerD
 
     public void informaData(View v){
         iniciarData();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(ano, mes, dia);
-        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
-            this,
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
         );
-        Calendar cMax = Calendar.getInstance();
-        Calendar cMin = Calendar.getInstance();
-        cMax.set(cMax.get(Calendar.YEAR),11,31);
-        datePickerDialog.setMaxDate(cMax);
-        datePickerDialog.setMaxDate(cMin);
-        List<Calendar> daysList = new LinkedList<>();
-        Calendar[] arrayDias;
-        Calendar cAux = Calendar.getInstance();
-        while(cAux.getTimeInMillis() <= cMax.getTimeInMillis()){
-            if(cAux.get(Calendar.DAY_OF_WEEK)!= Calendar.SUNDAY && cAux.get(Calendar.DAY_OF_WEEK)!=Calendar.SATURDAY){
-                    Calendar c = Calendar.getInstance();
-                    c.setTimeInMillis(cAux.getTimeInMillis());
-                    daysList.add(c);
-            }
-            cAux.setTimeInMillis(cAux.getTimeInMillis()+(24*60*60*1000));
-        }
-        arrayDias = new  Calendar[daysList.size()];
-        for (int i = 0; i < arrayDias.length;i++){
-            arrayDias[i] = daysList.get(i);
-        }
-        datePickerDialog.setSelectableDays(arrayDias);
-        datePickerDialog.setOnCancelListener(this);
-        datePickerDialog.show(getFragmentManager(),"DatePickerDialog");
+        dpd.show(getFragmentManager(), "Datepickerdialog");
     }
     public void iniciarData(){
         if(ano==0){
@@ -85,35 +61,15 @@ public class CadastroAtividades extends AppCompatActivity implements DatePickerD
 
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        Calendar tCalendar = Calendar.getInstance();
-        tCalendar.set(ano, mes, dia);
-            ano = year;
-            mes = monthOfYear;
-            dia = dayOfMonth;
-
-        TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(
-                this,
-                tCalendar.get(Calendar.HOUR_OF_DAY),
-                tCalendar.get(Calendar.MINUTE),
-                true
-        );
-        timePickerDialog.setOnCancelListener(this);
-        timePickerDialog.show(getFragmentManager(),"timePickerDialog");
+        String date = dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        texto_data.setText(date);
     }
 
     @Override
     public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
-            if(minute<9 || second >19){
-                onDateSet(null,ano,mes,dia);
-                Toast.makeText(this,"somente entre 9 e 19", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        horas = minute;
-        minutos = second;
+        String time = "You picked the following time: "+hourOfDay+"h"+minute+"m"+second;
+        //t.setText(time);
 
-        texto_data.setText((dia<10 ? "0"+dia:dia)+"/"+
-                           (mes+1<10?"0"+(mes+1):mes)+"/"+
-                            ano);
     }
 
     @Override
