4c40a69bba2c9bfe14cc84fdfae356c921cdd672
oliveira <thiago-coxim@hotmail.com>
2017-06-05 20:27:13 -0400

    'ListView Funcionando'

--- app/src/main/java/com/example/oliveira/salvaaluno/Tab_atividades.java
+++ app/src/main/java/com/example/oliveira/salvaaluno/Tab_atividades.java
@@ -19,7 +19,17 @@ public class Tab_atividades extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_listagem, container, false);
+        if(rootView!=null){
+            List<AtividadeObj> atividades;
+            AtividadeDAO pdao = new AtividadeDAO (getContext());
+            pdao.open();
+            atividades = pdao.getAll();
 
+            pdao.close();
+            ListAdapter adapter = new ListaAdapter(getContext(),atividades);
+            ListView lv = (ListView) rootView.findViewById(R.id.listagem);
+            lv.setAdapter(adapter);
+        }
         return rootView;
     }
 
