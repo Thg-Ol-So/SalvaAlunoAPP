1dbfff71458dfaf6ebb56d42cf8af6136c7c3d0e
oliveira <thiago-coxim@hotmail.com>
2017-06-05 20:04:39 -0400

    'ListView'

--- app/src/main/java/com/example/oliveira/salvaaluno/Tab_Trabalhos.java
+++ app/src/main/java/com/example/oliveira/salvaaluno/Tab_Trabalhos.java
@@ -1,16 +1,22 @@
 package com.example.oliveira.salvaaluno;
 
+/**
+ * Created by oliveira on 04/06/17.
+ */
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 
 public class Tab_Trabalhos extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3_trabalhos, container, false);
 
-        return inflater.inflate(R.layout.tab3_trabalhos, container, false);
+        return rootView;
     }
 }
