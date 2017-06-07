e16ead8a7b9087d4694649091dd2dae774eedbdd
Luciano Freire <lucianofreire.freire@gmail.com>
2017-06-06 20:11:30 -0400

    Adicionando Bordas as Caixa de Textos

--- app/src/main/java/com/example/oliveira/salvaaluno/Atividades.java
+++ app/src/main/java/com/example/oliveira/salvaaluno/Atividades.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class Atividades extends AppCompatActivity {
 
@@ -69,7 +70,19 @@ public class Atividades extends AppCompatActivity {
             return true;
         }
 
+        int sair = item.getItemId();
+
+        if (sair == R.id.acao_sair) {
+            finish();
+            toast("Aplicativo Encerrado");
+
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+    private void toast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
--- app/src/main/java/com/example/oliveira/salvaaluno/MainActivity.java
+++ app/src/main/java/com/example/oliveira/salvaaluno/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,4 +38,32 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_atividades, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.acao_sair) {
+            finish();
+            toast("Aplicativo Encerrado");
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
